730cf8f76abeb8e7b407eea3b51021a55e3a11a0
smismi <smirnov.ru@gmail.com>
2014-02-20 16:49:32 +0400

    callbacks autoswitcher

--- js/jquery.switcher.js
+++ js/jquery.switcher.js
@@ -1,40 +1,131 @@
 $.fn.switcher = function(options) {
 	var settings = $.extend({
-		delay: 300
+		delay: 300,
+		autoswitch:     false,
+		saveposition:   false,
+		beforeOpen:     false,
+		afterOpen:      false,
+		beforeClose:    false,
+		afterClose:     false,
+		onSuccess:      false,
+		onComplete:     false
 	}, options);
 
 	return this.each(function() {
 		var prefix = settings.prefix,
 			delay = settings.delay,
+			autoswitch = settings.autoswitch,
+			saveposition = settings.saveposition,
+			beforeOpen = settings.beforeOpen,
+			afterOpen = settings.afterOpen,
+			beforeClose = settings.beforeClose,
+			afterClose = settings.afterClose,
+			onSuccess = settings.onSuccess,
+			onComplete = settings.onComplete,
 			block = $(this),
 			button = block.find('.' + prefix + '_expand'),
+			header = block.find('.' + prefix + '_header'),
 			content = block.find('.' + prefix + '_content');
 
 
 		button.on("click", function() {
-			if (!block.hasClass(prefix + '_saved') ) {
+			if (saveposition && !block.hasClass(prefix + '_saved') ) {
 				block.addClass(prefix + '_saved');
+				saveState(prefix, "opened");
 				return false;
 			}
 			if ( content.filter(':visible').length > 0) {
+
+				if (beforeClose) {
+					beforeClose();
+				}
 
 				content.slideUp(delay, function() {
 					block.addClass(prefix + '_closed').removeClass(prefix + '_opened');
 					$(this).css('display','');
 
+					if (afterClose) {
+						afterClose();
+					}
+					if (onComplete) {
+						onComplete();
+					}
 				});
 
+				if (saveposition) saveState(prefix, "");
+
 			} else {
+
+				if (beforeOpen) {
+					beforeOpen();
+				}
+
 
 				content.slideDown(delay, function() {
 					block.removeClass(prefix + '_closed').addClass(prefix + '_opened');
 					$(this).css('display','');
 
-				});
- 			}
+					if (afterOpen) {
+						afterOpen();
+					}
 
+					if (onComplete) {
+						onComplete();
+					}
+				});
+
+				if (saveposition) saveState(prefix, "opened");
+
+			}
+
+			// if state change by user autoswitch off
+
+			if (autoswitch) {
+				header.off("mouseenter");
+			}
+			if (onSuccess) {
+				onSuccess();
+			}
 			return false;
 		});
 
+
+//		if option mouseenter switch
+		if (autoswitch) {
+			header.on("mouseenter", function() {
+				if( block.hasClass(prefix + '_closed') && !block.hasClass(prefix + '_saved') ) {
+
+					if (beforeOpen) {
+						beforeOpen();
+					}
+
+					content.slideDown(delay, function() {
+						block.removeClass(prefix + '_closed').addClass(prefix + '_opened');
+						$(this).css('display','');
+
+
+						if (afterOpen) {
+							afterOpen();
+						}
+
+					});
+				}
+			});
+		}
+
+
+
+
 	});
+
+	//		save state in   js-store
+
+	function saveState (block, state){
+		console.log('saveState');
+//		$.post('/services/js_store', {
+//			action:"save",
+//			"data":'{"switch":{"'+ block +'":"'+ state +'"}}'
+//		});
+	}
+
 };
